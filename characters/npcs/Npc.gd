322ce17c90d2cb2d55b63db3fb77cdcaecaddc5a
Tobias Zelders <tobias.zelders@epitech.eu>
2026-02-04 05:28:26 +0100

    bug: fixed npc wandering

--- characters/npcs/Npc.gd
+++ characters/npcs/Npc.gd
@@ -12,22 +12,10 @@ func _ready():
 
 func _start_wandering() -> void:
 	while true:
-		# Pick random direction
-		direction = Vector3(
-			randf_range(-1, 1),
-			0,
-			randf_range(-1, 1)
-		).normalized()
-
-		# Move for a while
+		direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
 		await get_tree().create_timer(move_time).timeout
-
-		# Stop moving
 		direction = Vector3.ZERO
-
-		# Wait before moving again
 		await get_tree().create_timer(wait_time).timeout
 
 func _process(delta):
-	# Move NPC
 	translate(direction * speed * delta)
